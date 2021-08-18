5fef0eb167a17ad1bc495ca2fddd83622041db9a
Ro <wondaero@nate.com>
2021-08-18 21:02:58 +0900

    롤백

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	// publicPath: 'https://wondaero.github.io/quiz/',	//배포할때 on, 개발할때 off
+	publicPath: 'https://wondaero.github.io/quiz/',	//배포할때 on, 개발할때 off
 	// devServer: {
 	// 	host: 'localhost',
 	// },
